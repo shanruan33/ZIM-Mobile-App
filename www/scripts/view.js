0aa25b0ec711b02365597c5bd374208b4c5f1b9b
Shan <shanruan333@gmail.com>
2020-04-22 13:16:11 -0400

    update page transition

--- www/scripts/view.js
+++ www/scripts/view.js
@@ -110,10 +110,10 @@ var app = function (app) {
 
             // page transition
 
-            const pages = new Pages([
-                { page: page0, swipe: [null, null, page1, page1] },
-                { page: page1, swipe: [null, null, null, null] }
-            ], "slide").addTo();
+            const pages = new Pages([page0,page1
+                // { page: page0, swipe: [null, null, page1, page1] },
+                // { page: page1, swipe: [null, null, null, null] }
+            ], "fade").addTo();
 
 
             v.manager.add(pages);
